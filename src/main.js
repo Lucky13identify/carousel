0e4dc187fa67f0adcc1634eaebf467df6642538f
Lucky13identify <108291368+Lucky13identify@users.noreply.github.com>
2023-11-15 22:40:05 +0200

    new

--- src/main.js
+++ src/main.js
@@ -25,7 +25,7 @@ localStorage.setItem("currentLanguage", i18n.global.locale);
 
 const router = createRouter({
   history: createWebHashHistory(),
-  base: "/",
+  base: "/MyCV/",
   routes: [
     { name: "Home", path: "/", component: MainPage },
     { name: "About", path: "/about", component: AboutMePage },
